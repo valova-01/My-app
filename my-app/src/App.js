d77a139bf16a2c2fc3728dc58df6d33304c5d294
anna <anna.valova.01@mail.ru>
2023-11-11 11:44:47 +0300

    Imperative or declarative code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
+	const currentYear = new Date().getFullYear(); // декларативный код начинется здесь
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -9,15 +10,13 @@ export const App = () => {
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Learn React
 				</a>
 			</header>
+			<footer>
+				<p>Текущий год: {currentYear}</p> {/* декларативный код заканчивается здесь*/}
+			</footer>
 		</div>
 	);
 };
